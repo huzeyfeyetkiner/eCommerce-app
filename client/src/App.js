aa3b158e218c90f3ef7661867b7fd45292c06eb2
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2023-05-30 17:32:45 +0300

    routing, products and card template

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Index";
+import Signup from "./pages/Auth/Signup";
+import Signin from "./pages/Auth/Sigin";
+import Products from "./pages/Products";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+
+      <div id="content">
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Products/index.js
@@ -0,0 +1,17 @@
+import Card from "../../components/Card";
+import { Grid } from "@chakra-ui/react";
+
+function Products() {
+  return (
+    <div>
+      <Grid templateColumns="repeat(5, 1fr)" gap={4}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </Grid>
+    </div>
+  );
+}
+export default Products;
